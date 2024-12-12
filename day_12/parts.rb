5c2988696b487efdc63829f0b08c1f2078e2c451
Ivo Dancet <ivo.dancet@by2.be>
2024-12-12 21:06:12 +0100

    p2

--- day_12/parts.rb
+++ day_12/parts.rb
@@ -38,23 +38,64 @@ class Region
     @points.push(*points)
   end
 
+  def price
+    perimeter * surface
+  end
+
+  def discounted_price
+    sides * surface
+  end
+
+  private
+
   def perimeter
     @points.sum do |point|
-      4 - neighbor_count(point)
+      4 - neighbors_in_region(point).length
     end
   end
 
-  def neighbor_count(point)
-    list = [point - Point[1, 0], point - Point[0, 1], point + Point[1, 0], point + Point[0, 1]]
-    (list & @points).length
+  def sides
+    points_with_fences = @points
+      .flat_map { |p| fence_sides(p).map { |fence_side| [p, fence_side] } }
+
+    points_with_fences.group_by(&:last).sum do |fence_side, points_with_fences|
+      points = points_with_fences.map(&:first)
+
+      if fence_side == :left || fence_side == :right
+        points.group_by(&:x).sum do |x, points|
+          1 + points.map(&:y).sort.each_cons(2).map { |y1, y2| y2 - y1 }.reject { |diff| diff == 1 }.length
+        end
+      else
+        points.group_by(&:y).sum do |y, points|
+          1 + points.map(&:x).sort.each_cons(2).map { |x1, x2| x2 - x1 }.reject { |diff| diff == 1 }.length
+        end
+      end
+    end
   end
 
   def surface
     @points.length
   end
 
-  def price
-    perimeter * surface
+  def neighbors_in_region(point)
+    potential_neighbors = {
+      (point - Point[1, 0]) => :left,
+      (point - Point[0, 1]) => :down,
+      (point + Point[1, 0]) => :right,
+      (point + Point[0, 1]) => :up,
+    }
+    potential_neighbors.keep_if { |point, _| @points.include?(point) }
+  end
+
+  def fence_sides(point)
+    %i[left down right up] - neighbors_in_region(point).values
+  end
+end
+
+class Fence
+  def initialize(point, direction)
+    @point = point
+    @direction = direction
   end
 end
 
@@ -65,7 +106,15 @@ module Day12
       map.regions.sum(&:price)
     end
   end
+
+  class Part2 < Part
+    def solve
+      map = Garden.new(@input)
+      map.regions.sum(&:discounted_price)
+    end
+  end
 end
 
 puts
-puts Day12::Part1.from_input_file.solve
+# puts Day12::Part1.from_input_file.solve
+# puts Day12::Part2.from_input_file.solve
--- day_12/parts_spec.rb
+++ day_12/parts_spec.rb
@@ -39,3 +39,52 @@ describe Day12::Part1 do
     end
   end
 end
+
+describe Day12::Part2 do
+  def do_action(input)
+    described_class.new(input).solve
+  end
+
+  maps_w_price = [
+    [
+      <<~MAP,
+        AAAA
+        BBCD
+        BBCC
+        EEEC
+      MAP
+      80,
+    ],
+    [
+      <<~MAP,
+        EEEEE
+        EXXXX
+        EEEEE
+        EXXXX
+        EEEEE
+      MAP
+      236,
+    ],
+    [
+      <<~MAP,
+        RRRRIICCFF
+        RRRRIICCCF
+        VVRRRCCFFF
+        VVRCCCJFFF
+        VVVVCJJCFE
+        VVIVCCJJEE
+        VVIIICJJEE
+        MIIIIIJJEE
+        MIIISIJEEE
+        MMMISSJEEE
+      MAP
+      1206,
+    ],
+  ]
+
+  maps_w_price.each_with_index do |(map, price), i|
+    it "handles #{map}" do
+      expect(do_action(map)).to eq(price)
+    end
+  end
+end
